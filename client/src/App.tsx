ad1c7cb0a3cf03a6a715304e20da241202e92b6a
alvarojpr <alvarojosedm9@gmail.com>
2024-06-27 11:48:36 -0300

    rota recuperar arquivos e front com campo diferente

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,37 +4,23 @@ import { Input } from "./components/ui/input";
 import { Button } from "./components/ui/button";
 
 export function App() {
-  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [file, setFile] = useState<File | null>(null);
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      setFile(e.target.files[0]);
-    }
-  };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const formData = new FormData();
-    formData.append("nome", name);
-    formData.append("email", email);
-    if (file) {
-      formData.append("file", file);
-    }
-
     try {
-      const response = await fetch("http://127.0.0.1:8000/upload", {
-        method: "POST",
-        body: formData,
-        headers: {},
+      const response = await fetch(`http://127.0.0.1:8000/simplesmenteget?email=${email}`, { // fetch é para fazer requisições HTTP
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
       });
 
       if (!response.ok) {
         throw new Error("Network response was not ok");
       } else {
-        const responseData = await response.json();
+        const responseData = await response.json(); // Transforma a resposta em JSON
         console.log(responseData);
       }
     } catch (error) {
@@ -48,20 +34,9 @@ export function App() {
         <h1 className="text-3xl font-bold text-center">Formulário</h1>
         <form className="gap-y-6" onSubmit={handleSubmit}>
           <div className="pb-4">
-            <Label>Nome</Label>
-            <Input className="mt-1" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-          </div>
-
-          <div className="pb-4">
             <Label>E-mail</Label>
             <Input className="mt-1" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
           </div>
-
-          <div className="pb-4">
-            <Label className="pb-20" htmlFor="file">Envie seu arquivo Evtx</Label>
-            <Input className="mt-1" id="file" type="file" onChange={handleFileChange} />
-          </div>
-
           <div>
             <Button type="submit" className="w-full">Enviar</Button>
           </div>
@@ -70,3 +45,81 @@ export function App() {
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+// export function App() {
+//   const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [file, setFile] = useState<File | null>(null);
+
+//   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     if (e.target.files && e.target.files.length > 0) {
+//       setFile(e.target.files[0]);
+//     }
+//   };
+
+//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+
+//     const formData = new FormData();
+//     formData.append("nome", name);
+//     formData.append("email", email);
+//     if (file) {
+//       formData.append("file", file);
+//     }
+
+//     try {
+//       const response = await fetch("http://127.0.0.1:8000/upload", {
+//         method: "POST",
+//         body: formData,
+//         headers: {},
+//       });
+
+//       if (!response.ok) {
+//         throw new Error("Network response was not ok");
+//       } else {
+//         const responseData = await response.json();
+//         console.log(responseData);
+//       }
+//     } catch (error) {
+//       console.error("There was a problem with the fetch operation:", error);
+//     }
+//   };
+
+//   return (
+//     <div className="w-full h-screen flex justify-center items-center bg-black">
+//       <div className="border border-black p-20 rounded-3xl shadow-2xl bg-white">
+//         <h1 className="text-3xl font-bold text-center">Formulário</h1>
+//         <form className="gap-y-6" onSubmit={handleSubmit}>
+//           <div className="pb-4">
+//             <Label>Nome</Label>
+//             <Input className="mt-1" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+//           </div>
+
+//           <div className="pb-4">
+//             <Label>E-mail</Label>
+//             <Input className="mt-1" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+//           </div>
+
+//           <div className="pb-4">
+//             <Label className="pb-20" htmlFor="file">Envie seu arquivo Evtx</Label>
+//             <Input className="mt-1" id="file" type="file" onChange={handleFileChange} />
+//           </div>
+
+//           <div>
+//             <Button type="submit" className="w-full">Enviar</Button>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
--- server/app/main.py
+++ server/app/main.py
@@ -1,49 +1,64 @@
-from fastapi import FastAPI, File, Form, HTTPException, UploadFile
+from typing import Optional # Optional serve para indicar que um argumento pode ser None 
+from fastapi import FastAPI, File, Form, HTTPException, UploadFile, Query # adicionado Query para recuperar o email do usuário e UploadFile para recuperar o arquivo    
 from fastapi.middleware.cors import CORSMiddleware
 import psycopg2
-
-def criar_tabela():
-    conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres", password="123", port="5433")
-    cur = conn.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS pessoa (
-        id SERIAL PRIMARY KEY,
-        nome VARCHAR(100),
-        email VARCHAR(100),
-        file BYTEA
-    )""")
-    conn.commit()
-    cur.close()
-    conn.close()
-
-criar_tabela()
+from evtx import PyEvtxParser
+from io import BytesIO # BytesIO é um buffer de memória que contém bytes de dados que podem ser lidos e gravados como um arquivo binário
 
 app = FastAPI()
 
-origins = [ # Lista de URLs que podem acessar a API
-    "http://localhost:5173",
-]
-
-app.add_middleware( # Middleware para permitir requisições de origens diferentes
+app.add_middleware(
     CORSMiddleware, 
-    allow_origins=origins,
-    allow_credentials=True, # Permite credenciais
-    allow_methods=["*"], # Permite todos os métodos (get, post, delete, etc.)
-    allow_headers=["*"], # Permite todos os cabeçalhos
+    allow_origins=["*"], # permitir que qualquer origem acesse a API
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-# Função para conectar ao banco de dados
 def conectar_bd():
-    conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres", password="123", port="5433")
-    return conn # connection
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            dbname="postgres",
+            user="postgres",
+            password="123",
+            port="5433"
+        )
+        return conn
+    except Exception as e:
+        print(f"Erro ao conectar ao banco de dados: {str(e)}")
+        raise HTTPException(status_code=500, detail="Erro ao conectar ao banco de dados")
 
-# Função para inserir dados na tabela pessoa
+def criar_tabela():
+    try:
+        conn = conectar_bd()
+        cur = conn.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS pessoa (
+            id SERIAL PRIMARY KEY,
+            nome VARCHAR(100),
+            email VARCHAR(100),
+            file BYTEA
+        )""")
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Exception as e:
+        print(f"Erro ao criar tabela: {str(e)}")
+        raise HTTPException(status_code=500, detail="Erro ao criar tabela")
+
+criar_tabela()
+
 def inserir_pessoa(nome, email, file):
-    conn = conectar_bd()
-    cur = conn.cursor() # cursor da conexão para executar comandos SQL
-    cur.execute("INSERT INTO pessoa (nome, email, file) VALUES (%s, %s, %s)", (nome, email, file))
-    conn.commit()
-    cur.close()
-    conn.close()
+    try:
+        conn = conectar_bd()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO pessoa (nome, email, file) VALUES (%s, %s, %s)", (nome, email, file))
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Exception as e:
+        print(f"Erro ao inserir pessoa: {str(e)}")
+        raise HTTPException(status_code=500, detail="Erro ao inserir pessoa")
 
 @app.post("/upload", status_code=201)
 async def criar_pessoa(nome: str = Form(...), email: str = Form(...), file: UploadFile = File(None)):
@@ -54,6 +69,59 @@ async def criar_pessoa(nome: str = Form(...), email: str = Form(...), file: Uplo
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Erro ao inserir pessoa: {str(e)}")
 
+@app.get("/simplesmenteget", status_code=201)
+def recuperarArquivo(email: Optional[str] = Query(None)):
+    if not email:
+        raise HTTPException(status_code=422, detail="Email is required")
+
+    try:
+        conn = conectar_bd()
+        cur = conn.cursor()
+    except Exception as e:
+        print(f"Erro ao conectar ao banco de dados: {str(e)}")
+        raise HTTPException(status_code=500, detail="Erro ao conectar ao banco de dados")
+
+    try:
+        cur.execute("SELECT * FROM pessoa WHERE email=%s", (email,))
+        pessoa = cur.fetchone()
+
+        if pessoa is None:
+            print(f"Nenhuma pessoa encontrada com o email: {email}")
+            raise HTTPException(status_code=404, detail="Pessoa não encontrada")
+
+        arquivo_da_pessoa_em_binario = pessoa[3]
+
+        if arquivo_da_pessoa_em_binario is None:
+            print(f"Arquivo da pessoa {email} não encontrado")
+            raise HTTPException(status_code=404, detail="Arquivo não encontrado")
+
+        # Usar BytesIO para criar um objeto de arquivo a partir dos bytes
+        arquivo_transformado_em_objeto = BytesIO(arquivo_da_pessoa_em_binario)
+        conjunto_de_registros_do_evtx = PyEvtxParser(arquivo_transformado_em_objeto)
+        
+        # .records_json(): Este método retorna um iterador que gera os registros do arquivo EVTX em formato JSON. Cada registro é um objeto JSON que 
+        # representa uma entrada no arquivo EVTX. ---------------------
+        # [registro for registro in ...]: é uma forma concisa de criar uma lista. Neste caso, ele está iterando sobre cada registro gerado pelo método 
+        # records_json() e adicionando-o à lista registros. ------------------------
+        # registros: será uma lista que contém todos os registros do arquivo EVTX em formato JSON.
+        # em outras palavras: cria uma lista com cada registro json do arquivo evtx
+        registros = [registro for registro in conjunto_de_registros_do_evtx.records_json()]
+        print(f"Quantidade de registros recuperados: {len(registros)}")
+        return {"registros": registros}
+
+    except Exception as e:
+        print(f"Erro ao recuperar arquivo: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Erro ao recuperar arquivo: {str(e)}")
+
+    finally:
+        cur.close()
+        conn.close()
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+
+# 200 sucesso, 300 redirecionamento, 400 erro do cliente, 500 erro do servidor
+# redirecionamento é quando o servidor diz para o cliente ir para outro lugar
